e68264a005a6c4d407c9620058e635ba5aa7ec5e
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2020-06-01 19:20:32 -0700

    First Java task

---
+++ HelloJava.java
@@ -0,0 +1,15 @@
+/*
+Output Format
+You must print two lines of output:
+    Print "Hello, World." on the first line.
+    Print "Hello, Java." on the second line.
+*/
+
+public class Solution {
+   
+        public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}

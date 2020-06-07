2197ca7d587685cef1afda0065125e4277404e7f
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2020-06-07 13:19:55 -0700

    Java Stdin and Stdout II

---
+++ StandartInputOutput2.java
@@ -0,0 +1,29 @@
+/*
+In this challenge, you must read an integer, a double, and a String from stdin, then print the values according 
+to the instructions in the Output Format section below. 
+Input Format. There are three lines of input:
+-The first line contains an integer.
+-The second line contains a double.
+-The third line contains a String.
+Output format. There are three lines of output:
+-On the first line, print String: followed by the unaltered String read from stdin.
+-On the second line, print Double: followed by the unaltered double read from stdin.
+-On the third line, print Int: followed by the unaltered integer read from stdin.
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        String empty = scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}

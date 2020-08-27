63917e224a51f527b62f5fa86986c5e1b440c9ce
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2020-08-26 21:38:19 -0700

    CamelCase

---
+++ Problem Solving/CamelCase.java
@@ -0,0 +1,41 @@
+/*
+https://www.hackerrank.com/challenges/camelcase/problem
+ */
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution {
+
+    // Complete the camelcase function below.
+    static int camelcase(String s) {
+        int counter = 1;
+        for (int i =0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if (Character.isUpperCase(ch)){
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
